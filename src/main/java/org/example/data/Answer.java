dc882731b8081bda496832968b1eb65746234d6a
kubq01 <kuba18062001@gmail.com>
2022-12-15 10:54:23 +0100

    completed Answer class

--- src/main/java/org/example/data/Answer.java
+++ src/main/java/org/example/data/Answer.java
@@ -1,4 +1,42 @@
 package org.example.data;
 
-public class Answer {
+import java.io.*;
+
+public class Answer implements Serializable {
+    private int studentID;
+    private int questionID;
+    private String answer;
+
+    public Answer(int studentID, int questionID, String answer) {
+        this.studentID = studentID;
+        this.questionID = questionID;
+        this.answer = answer;
+    }
+
+    public int getStudentID() {
+        return studentID;
+    }
+
+    public int getQuestionID() {
+        return questionID;
+    }
+
+    public String getAnswer() {
+        return answer;
+    }
+
+    @Override
+    public String toString() {
+        return String.valueOf(
+                studentID +
+                ";" + questionID +
+                ";" + answer);
+    }
+
+    public synchronized void saveToFile() throws IOException {
+        FileWriter fileWriter = new FileWriter(new File("bazaOdpowiedzi.txt"), true);
+        BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+        bufferedWriter.write(toString());
+        bufferedWriter.close();
+    }
 }
